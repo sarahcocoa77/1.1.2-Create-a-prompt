228383457ae8232559749854ce321bb8a9168017
sarahcocoa77 <sarahcocoa777@gmail.com>
2026-05-12 04:35:42 +0000

    create prompt and alert

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Write your code for Part 1 below
-
+prompt("what is your favorite color?")
 
 
 
 // Then, after your tried a prompt yourself,
 // uncomment the two lines below to try Part 2
 
-// var firstName = prompt('What is your name?');
-// alert('Welcome ' + firstName + '!');
+ var firstName = prompt('What is your name?');
+ alert('Welcome ' + firstName + '!');
